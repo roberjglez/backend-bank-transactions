c125da520d77eaf49bbc34d9a7059607fcc4bbbb
Rober <roberto.jesus.gonzalez.carrato.pozo.ext@ing.com>
2020-06-22 16:32:31 +0200

    First commit

---
+++ src/main/java/com/rjglez/backend/bank/transactions/BackendBankTransactionsApplication.java
@@ -0,0 +1,13 @@
+package com.rjglez.backend.bank.transactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendBankTransactionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendBankTransactionsApplication.class, args);
+	}
+
+}

8d0e068bc594443d5aefd1d5d59da295710449b9
Rober <roberto.jesus.gonzalez.carrato.pozo.ext@ing.com>
2020-07-23 13:01:29 +0200

    Added maven-surefire-plugin to execute tests from terminal

--- src/test/java/com/rjglez/backend/bank/transactions/BackendBankTransactionsApplicationTests.java
+++ src/test/java/com/rjglez/backend/bank/transactions/BackendBankTransactionsApplicationTests.java
@@ -3,7 +3,7 @@ package com.rjglez.backend.bank.transactions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = {BackendBankTransactionsApplication.class})
 class BackendBankTransactionsApplicationTests {
 
     @Test
